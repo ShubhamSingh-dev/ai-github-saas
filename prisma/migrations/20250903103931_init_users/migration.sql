a816e118026a2db42564f3a9f1344a51aceb7834
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-09-03 16:11:29 +0530

    Started adding Auth

---
+++ prisma/migrations/20250903103931_init_users/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."Post";
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "imageUrl" TEXT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "emailAddress" TEXT NOT NULL,
+    "credits" INTEGER NOT NULL DEFAULT 150,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_emailAddress_key" ON "public"."User"("emailAddress");
