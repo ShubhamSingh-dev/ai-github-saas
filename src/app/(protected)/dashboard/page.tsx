4008819a12d72009f7165093f077a5d22edec399
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-09-05 14:44:07 +0530

    Strated with dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,12 +1,41 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import useProject from "~/hooks/use-project";
 
 const DashboardPage = () => {
-  const { user } = useUser();
+  const { project } = useProject();
   return (
     <div>
-      <h1>{user?.firstName}</h1>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        {/* Github Link */}
+        <div className="bg-primary w-fit rounded-md px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This Project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">Tea</div>
+      </div>
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">CArd</div>
+      </div>
+      <div className="mt-8"></div>
+      Commit Log
     </div>
   );
 };
